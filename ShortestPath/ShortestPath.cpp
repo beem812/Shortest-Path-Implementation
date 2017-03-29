bd9b49718f924b02bf6facf5887f8493553ee679
James Beem <beem132@gmail.com>
2017-03-29 08:45:11 -0400

    Update ShortestPath.cpp
    formatting some comments

--- ShortestPath/ShortestPath.cpp
+++ ShortestPath/ShortestPath.cpp
@@ -14,9 +14,9 @@ int main()
 	path.processNodes(); //Find shortest path for all nodes
 	path.generateImage(); //writing the approach directions back to a raw file output = map2.raw
 	//map2.raw is formatted such that a byte value of: 50 = approached from the north
-													//100 = approached from the east
-													//150 = approched form the south
-													//200 = approached from the west
+					                 //100 = approached from the east
+						         //150 = approched form the south
+							 //200 = approached from the west
 	path.markShortPath(); //drawing shortest path from [0][0] to [199][249] output = path.raw
     return 0;
 }
